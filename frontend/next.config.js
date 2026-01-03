121bc65852b21eee9f75fde46b3ef122b9f277e6
Viktor Taseski <vtaseski24@gmail.com>
2026-01-03 07:33:10 +0100

    migration fix 02

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,4 +4,12 @@ const nextConfig = {
   output: 'standalone',
 };
 
+if (process.env.DEBUG_BUILD === 'true') {
+  // eslint-disable-next-line no-console
+  console.log('[build] next.config', {
+    output: nextConfig.output,
+    nodeEnv: process.env.NODE_ENV,
+  });
+}
+
 module.exports = nextConfig;
